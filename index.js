d752c1dac38e647b56fc6352a975d1248f465bc3
Timothy Ryon <tim@timryon.com>
2023-05-14 21:02:48 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "tim";
+console.log(leastFavoriteCustomer);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+} 
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+    console.log(leastFavoriteCustomer);
+}
